1db0cabf3ac174fc6ff5767ad912b086ec879e0b
Pharmatools <raphiphat.13495@gmail.com>
2026-04-13 00:04:11 +0700

    Add files via upload

--- Assets/JS/firebase-config.js
+++ Assets/JS/firebase-config.js
@@ -1,18 +1,21 @@
-// ไฟล์: Assets/JS/firebase-config.js
-
-// 1. นำเข้าคำสั่งของ Firebase จาก CDN
+// Assets/JS/firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getDatabase, ref, set, onValue, remove, push, get, child } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
+import { getAuth, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
-// 2. ใส่ URL ของฐานข้อมูลที่คุณเพิ่งสร้าง
 const firebaseConfig = {
-  // ⚠️ เปลี่ยนลิงก์ด้านล่างให้เป็น URL ของคุณ (อย่าลืมเครื่องหมายคำพูด "")
-  databaseURL: "https://pharmatoolsthailand-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  apiKey: "AIzaSyBZwewZHH7ThJU5XBIwyERIvkgXaL3OF84",
+  authDomain: "pharmatoolsthailand.firebaseapp.com",
+  databaseURL: "https://pharmatoolsthailand-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "pharmatoolsthailand",
+  storageBucket: "pharmatoolsthailand.firebasestorage.app",
+  messagingSenderId: "164301600139",
+  appId: "1:164301600139:web:f5780cb1ade7f9c6026b8e",
+  measurementId: "G-TRKNM90D8L"
 };
 
-// 3. เริ่มต้นเปิดการเชื่อมต่อ
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
+const auth = getAuth(app);
 
-// ส่งออกคำสั่งต่างๆ ไปให้ไฟล์อื่นใช้งาน
-export { db, ref, set, onValue, remove, push, get, child };
+export { db, ref, set, onValue, remove, push, get, child, auth, signInWithEmailAndPassword, signOut, onAuthStateChanged };
